1bb2aa975b5a40a4e3b12f77c3fb36a76410f084
Aaron Wiggins <apw7177@gmail.com>
2015-06-09 18:46:16 -0400

    add teacher controller. teacher can edit email and password

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,27 @@
 class TeachersController < ApplicationController
+  before_action :set_teacher, only: [:edit, :update]
+
   def edit
-    
   end
+
+  def update
+    respond_to do |format|
+      if @teacher.update(teacher_params)
+        format.html { redirect_to parents_path, notice: 'Teacher was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_teacher
+      @teacher = Teacher.find_by_id(session[:teacher_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def teacher_params
+      params.require(:teacher).permit(:name, :email, :password)
+    end
 end
--- test/controllers/teachers_controller_test.rb
+++ test/controllers/teachers_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class TeachersControllerTest < ActionController::TestCase
   setup do
-    @parent = parents(:one)
+    @teacher = teachers(:one)
     bill = Teacher.create!(name: "bill", email: "bill@bill", password: "bill")
     session[:teacher_id] = bill.id
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @teacher
     assert_response :success
   end
 
